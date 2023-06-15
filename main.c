e439acedd143c9e4496cd6534cc37a0ea632b0ca
Adam <asoliyev13@gmail.com>
2023-06-15 02:00:11 -0500

    add + and - operators

--- main.c
+++ main.c
@@ -3,13 +3,32 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        fprintf(stderr, "%s: Invalid number of arguments\n", argv[0]);
         return 1;
     }
 
+    char *p = argv[1];
+
     printf("    .globl main\n");
     printf("main:\n");
-    printf("   li a0, %d\n", atoi(argv[1]));
+    printf("   li a0, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("   addi a0, a0, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("   addi a0, a0, -%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "Unexpected character: '%c'\n", *p);
+        return 1;
+    }
     printf("   ret\n");
     return 0;
 }
--- test.sh
+++ test.sh
@@ -7,6 +7,7 @@ assert() {
     ./luan "$input" > tmp.s || exit
 
     riscv64-unknown-linux-gnu-gcc -static -o tmp tmp.s
+
     qemu-riscv64 -L /home/adam/dev/riscv/sysroot ./tmp
 
     actual="$?"
@@ -21,5 +22,6 @@ assert() {
 
 assert 0 0
 assert 32 32
+assert 21 "5+20-4"
 
 echo OK
